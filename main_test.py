7be7f154a3f36584770a5453d7976cfce933b213
romanshchuka <roman.shchuka@wide.vision>
2019-01-14 14:00:30 +0200

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -4,35 +4,6 @@ import psycopg2.pool
 import sqlalchemy
 
 
-def test_main():
-    import main_mysql
-    main_mysql.pymysql = MagicMock()
-    fetchall_mock = main_mysql.pymysql.connect().cursor().__enter__().fetchall
-    fetchall_mock.return_value = [['0']]
-
-    main_mysql.app.testing = True
-    client = main_mysql.app.test_client()
-
-    r = client.get('/')
-    assert r.status_code == 200
-    assert '0' in r.data.decode('utf-8')
-
-
-def test_main_pooling():
-    sqlalchemy.create_engine = MagicMock()
-
-    import main_mysql_pooling
-
-    cnx_mock = main_mysql_pooling.sqlalchemy.create_engine().connect()
-    cnx_mock.execute().fetchall.return_value = [['0']]
-
-    main_mysql_pooling.app.testing = True
-    client = main_mysql_pooling.app.test_client()
-
-    r = client.get('/')
-    assert r.status_code == 200
-    assert '0' in r.data.decode('utf-8')
-
 
 def test_main_postgressql():
     import main_postgres
