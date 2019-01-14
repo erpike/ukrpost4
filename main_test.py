c5e6e2040a0d4bb58af02e6cee443d0712da3e85
romanshchuka <roman.shchuka@wide.vision>
2019-01-14 14:12:54 +0200

    upd4

--- main_test.py
+++ main_test.py
@@ -6,13 +6,13 @@ import sqlalchemy
 
 
 def test_main_postgressql():
-    import main_postgres
-    main_postgres.psycopg2.connect = MagicMock()
-    mock_cursor = main_postgres.psycopg2.connect().cursor()
+    import main
+    main.psycopg2.connect = MagicMock()
+    mock_cursor = main.psycopg2.connect().cursor()
     mock_cursor.__enter__().fetchall.return_value = [['0']]
 
-    main_postgres.app.testing = True
-    client = main_postgres.app.test_client()
+    main.app.testing = True
+    client = main.app.test_client()
 
     r = client.get('/')
     assert r.status_code == 200
@@ -22,13 +22,13 @@ def test_main_postgressql():
 def test_main_postgressql_pooling():
     psycopg2.pool.ThreadedConnectionPool = MagicMock()
 
-    import main_postgres_pooling
+    import main
 
-    mock_pool = main_postgres_pooling.psycopg2.pool.ThreadedConnectionPool()
+    mock_pool = main.psycopg2.pool.ThreadedConnectionPool()
     mock_pool.getconn().cursor().__enter__().fetchall.return_value = [['0']]
 
-    main_postgres_pooling.app.testing = True
-    client = main_postgres_pooling.app.test_client()
+    main.app.testing = True
+    client = main.app.test_client()
 
     r = client.get('/')
     assert r.status_code == 200
